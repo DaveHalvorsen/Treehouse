ad95308269f7e7a3e304021d8580a079bd81c2db
David Halvorsen <daviqd.halvorsen@gmail.com>
2018-05-08 12:35:20 -0700

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,8 @@
+# This function accepts a # and checks if it's even
+# The check is done with modulo for 2. If modulo 2
+# is equivalent to 0 (evenly divisilbe by 2.
+def even_odd(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
