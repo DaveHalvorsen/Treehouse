edb14a643f908881fef03ac13d2a55e97683d683
David Halvorsen <daviqd.halvorsen@gmail.com>
2018-05-09 16:20:05 -0700

    Create favorite_things.py

---
+++ Python_Collections/favorite_things.py
@@ -0,0 +1,8 @@
+# This is a demonstration of list slicing
+favorite_things = ['raindrops on roses', 'whiskers on kittens', 'bright copper kettles',
+                   'warm woolen mittens', 'bright paper packages tied up with string',
+                   'cream colored ponies', 'crisp apple strudels']
+slice1 = favorite_things[1:4]
+slice2 = favorite_things[-2:]
+sorted_things = favorite_things[:]
+sorted_things.sort()
