2b88c22a1856a7edd829b8d9f53ff9204a2f6d71
David Halvorsen <daviqd.halvorsen@gmail.com>
2018-05-09 23:07:40 -0700

    Create player_dictionary.py

---
+++ Python_Collections/player_dictionary.py
@@ -0,0 +1,2 @@
+# This is a demonstration of a dictionary within a dictionary
+player = {"name": "Rick Sanchez", "remaining_lives" : 3, "levels": [1,2,3,4], "items": {"weapon": "Golden Gun", "shield":"Hyrule Shield"}}
