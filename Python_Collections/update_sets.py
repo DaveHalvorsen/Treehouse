c71336ee9661448d1833ae1a0cb363962951f328
Dave Halvorsen <daviqd.halvorsen@gmail.com>
2018-05-18 17:43:11 -0700

    Create update_sets.py

---
+++ Python_Collections/update_sets.py
@@ -0,0 +1,4 @@
+# demonstration of sets and adding / updating them
+songs = {'Hey There Delilah', 'Rocky Racoon', 'While My Guitar Gently Weeps'}
+songs.add('Treehouse Hula')
+songs.update({"Python Two-Step", "Ruby Rhumba"}, {"My PDF Files"})
