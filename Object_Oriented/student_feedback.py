363dea2731324427562db79c6ec681e823e36f1d
Dave Halvorsen <daviqd.halvorsen@gmail.com>
2018-05-19 19:26:54 -0700

    Create student_feedback.py

---
+++ Object_Oriented/student_feedback.py
@@ -0,0 +1,21 @@
+# Here's the code challenge text for this exercise:
+# It should take an argument named grade. Methods take arguments 
+# just like functions do. You'll still need self in there, though. 
+# If grade is above 50, return the result of the praise method. 
+# If it's 50 or below, return the reassurance method's result.
+
+class Student:
+    name = "David"
+    
+    def praise(self):
+        return "You inspire me, {}".format(self.name)
+    
+    def reassurance(self):
+        return "Chin up, {}. You'll get it next time!".format(self.name)
+    
+    def feedback(self, grade):
+        if grade > 50:
+            return self.praise()
+        else:
+            return self.reassurance()
+        
